f5e8e1c71068bdd1a630996791228a00c5ff8c8a
Paula Burghelea <paulaburghelea.github@gmail.com>
2024-05-21 21:17:19 +0100

    mutation for adding, deleting and updating done

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,47 @@ const resolvers = {
         review: (_, args) => db.reviews.find((review) => review.id === args.id),
         game: (_, args) => db.games.find((game) => game.id === args.id),
         author: (_, args) => db.authors.find((author) => author.id === args.id)
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter(r => r.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(r => r.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find(a => a.id === parent.author_id);
+        },
+        game(parent) {
+            return db.games.find(g => g.id === parent.game_id);
+        }
+    },
+    Mutation: {
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            };
+            db.games.push(game);
+            return game;
+        },
+        deleteGame(_, args) {
+            db.games = db.games.filter(g => g.id != args.id);
+            return db.games;
+        },
+        updateGame(_, args) {
+            db.games = db.games.map(g => {
+                if (g.id === args.id) {
+                    return { ...g, ...args.edits };
+                }
+                return g;
+            });
+            return db.games.find(g => g.id === args.id);
+        }
     }
 };
 // The ApolloServer constructor requires two parameters: your schema
